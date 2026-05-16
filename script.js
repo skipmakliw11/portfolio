e027865114985ca622997523a33de2ccb9100f10
Daniel Gian Salinas <danielgiansalinas@gmail.com>
2026-05-16 09:23:11 +0800

    Adjust minor pages

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal(src) {
+    document.getElementById("certModal").style.display = "block";
+    document.getElementById("modalImg").src = src;
+}
+
+function closeModal() {
+    document.getElementById("certModal").style.display = "none";
+}
